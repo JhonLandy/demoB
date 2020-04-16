42b7500c1d5317f960a1140d13a1a48849c9f7f9
wb.yuanchenglang <wb.yuanchenglang@mesg.corp.netease.com>
2020-04-16 11:36:47 +0800

    as

--- router/router.js
+++ router/router.js
@@ -2,7 +2,6 @@ let pageRouter = require('./pageRouter')
 let loginRouter = require('./loginRouter')
 let tableRouter = require('./tableRouter')
 
-
 module.exports = function (app) {
     app.use(pageRouter.routes());
     app.use(loginRouter.routes());
